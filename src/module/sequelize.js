d7e746d57d38b463ad010e53b20077e3d320f8bc
mango906 <kirby@101.inc>
2021-02-28 00:38:04 +0900

    :sparkles: 모델들의 외래키를 설정해주었습니다

--- src/module/sequelize.js
+++ src/module/sequelize.js
@@ -1,4 +1,8 @@
 const Sequelize = require('sequelize');
+const User = require('../model/User');
+const Post = require('../model/Post');
+const Charge = require('../model/Charge');
+const Donate = require('../model/Donate');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
@@ -6,5 +10,20 @@ const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 db.sequelize = sequelize;
+
+db.User = User;
+db.Post = Post;
+db.Charge = Charge;
+db.Donate = Donate;
+
+User.init(sequelize);
+Post.init(sequelize);
+Charge.init(sequelize);
+Donate.init(sequelize);
+
+User.associate(db);
+Post.associate(db);
+Charge.associate(db);
+Donate.associate(db);
 
 module.exports = db;
