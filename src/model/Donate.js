c659461332474b8db1e103ada7bd59d6f2b72621
mango906 <kirby@101.inc>
2021-02-27 19:43:03 +0900

    :sparkles: Donate 모델을 생성하였습니다

---
+++ src/model/Donate.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Donate extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        donate_id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        user_id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false,
+          unique: true,
+        },
+        post_id: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false,
+        },
+        amount: {
+          type: Sequelize.STRING(45),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        paranoid: true,
+        charset: 'utf8',
+        modelName: 'Donate',
+        tableName: 'donate',
+      }
+    );
+  }
+  static associate(db) {
+    //TODO(@mango906): 외래키 연결해주기
+  }
+};
