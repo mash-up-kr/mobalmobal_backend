737e5c1c4d09faf799d592c21382a6c1caa1b08b
HyunLee <olo5112olo@naver.com>
2021-02-27 23:47:56 +0900

    :sparkles: DB 연결을 위한 sequelize 코드를 작성합니다 (#7)

--- src/module/sequelize.js
+++ src/module/sequelize.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const env = process.env.NODE_ENV || 'delvelopment';
-const config = require('../config/config')[env];
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.json')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
