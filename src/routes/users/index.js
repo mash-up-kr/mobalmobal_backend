0b11d92249a5ef59d3013891056a2e9e62bee7ab
Lee Sohyeong <69043819+sohyeong-dev@users.noreply.github.com>
2021-03-28 16:10:52 +0900

    ✨ 회원가입 API (#13)
    * ✨ 회원가입 API
    
    * :recycle: 함수화
    
    Co-authored-by: sohyeong-dev <so-hyeong@naver.com>

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require('../../controller/userController');
 
 router.post('/login', userController.login);
-// router.post('/', userController.create);
+router.post('/', userController.create);
 
 module.exports = router;
--- src/service/userService.js
+++ src/service/userService.js
@@ -10,6 +10,46 @@ const getByFireStoreId = async (fireStoreId) => {
   return user;
 };
 
+const isUniqueName = async (nickname) => {
+    const user = await User.findOne({
+        where: { nickname }
+    });
+    if (!user) return true;
+    return false;
+};
+
+const insertUser = async ({ nickname, cash, provider, fireStoreId, ...values }) => {
+
+  if (!await isUniqueName(nickname)) {
+    let error = new Error("이미 사용중인 닉네임입니다.");
+    error.name = "BAD_REQUEST";
+    throw error;
+  }
+
+  cash = cash || 0;
+
+  values = camelToSnakeCase(values);
+  
+  const user = await User.create({
+    nickname,
+    cash,
+    provider,
+    firestore_id: fireStoreId,
+    ...values
+  });
+  return user;
+};
+
 module.exports = {
   getByFireStoreId,
+  insertUser,
+};
+
+const camelToSnakeCase = (params) => {
+  return Object.entries(params).map(([k, v]) => {
+    return [k.replace(/[A-Z]/g, (match) => "_" + match.toLowerCase()), v];
+  }).reduce((acc, [k, v]) => {
+    acc[k] = v;
+    return acc;
+  }, {});
 };
