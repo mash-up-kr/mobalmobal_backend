4ff22a7612efab90d074f8c05f521a5a107a36a5
kirby <kirby@101.inc>
2021-04-04 17:23:28 +0900

    :sparkles: 내 post를 가져오는 api를 추가하였습니다 (#25)

--- src/routes/posts/index.js
+++ src/routes/posts/index.js
@@ -4,6 +4,7 @@ const postController = require('../../controller/postController');
 const auth = require('../../module/authUtils');
 
 router.get('/', postController.getAll);
+router.get('/my', auth.isLoggedin, postController.getMyPost);
 router.get('/:post_id', postController.getById);
 router.post('/', auth.isLoggedin, postController.addPost);
 
