41f7197e536aeb02a25c64233b0404ae5f24efb0
mango906 <kirby@101.inc>
2021-04-07 17:10:56 +0900

    :sparkles: 후원을 할 때 post의 current_amount도 같이 오르게 하였습니다

--- src/service/donateService.js
+++ src/service/donateService.js
@@ -1,7 +1,22 @@
 const Donate = require('../model/Donate');
+const Post = require('../model/Post');
 
 const createDonate = async (donate) => {
   const donateDoc = await Donate.create(donate);
+
+  const { amount, post_id } = donate;
+
+  await Post.increment(
+    {
+      current_amount: amount,
+    },
+    {
+      where: {
+        post_id,
+      },
+    }
+  );
+
   return donateDoc;
 };
 
