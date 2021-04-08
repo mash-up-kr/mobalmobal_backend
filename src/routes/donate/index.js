dd008ad6a043ddffe45689b7dfd0641494147c27
mango906 <kirby@101.inc>
2021-04-09 00:40:43 +0900

    :sparkles: 내 후원을 가져오는 api를 추가한다

--- src/routes/donate/index.js
+++ src/routes/donate/index.js
@@ -4,5 +4,6 @@ const auth = require('../../module/authUtils');
 const donateController = require('../../controller/donateController');
 
 router.post('/', auth.isLoggedin, donateController.createDonate);
+router.get('/my', auth.isLoggedin, donateController.myDonate);
 
 module.exports = router;
